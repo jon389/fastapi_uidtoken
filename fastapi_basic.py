d567a157e3914d8ec6c69795038349c497569ebf
jon389 <24356268+jon389@users.noreply.github.com>
2020-10-18 07:22:20 +0100

    re-write main module using db & ip auth

--- fastapi_basic.py
+++ fastapi_basic.py
@@ -2,6 +2,7 @@ from typing import Optional
 import base64
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
+from secrets import token_bytes
 
 import jwt
 from jwt import PyJWTError
@@ -11,33 +12,23 @@ from pydantic import BaseModel
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2
-from fastapi.security.base import SecurityBase
+from fastapi.security.base import SecurityBase, SecurityBaseModel
 from fastapi.security.utils import get_authorization_scheme_param
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
 from fastapi.openapi.utils import get_openapi
+from fastapi.logger import logger
 
 from starlette.status import HTTP_403_FORBIDDEN
 from starlette.responses import RedirectResponse, Response, JSONResponse
 from starlette.requests import Request
 
+from testdb import db
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
+JWT_ALGORITHM = 'HS256'
+JWT_SECRET_KEY = bytes.hex(token_bytes(32))
+JWT_ACCESS_TOKEN_EXPIRE_MINUTES = 30
+COOKIE_DOMAIN_NAME = 'desk4.home'
 
 
 class Token(BaseModel):
@@ -50,46 +41,44 @@ class TokenData(BaseModel):
 
 
 class User(BaseModel):
-    username: str
-    email: str = None
+    User: str
+    UID: str = None
+    Timestamp: datetime = None
     full_name: str = None
-    disabled: bool = None
+    email: str = None
+    label: str = None
 
 
 class UserInDB(User):
-    hashed_password: str
+    # hashed_password: str
+    disabled: bool = False
+    Machine: str = None
+    NetAddress: str = None
 
 
 class OAuth2PasswordBearerCookie(OAuth2):
     def __init__(
-        self,
-        tokenUrl: str,
-        scheme_name: str = None,
-        scopes: dict = None,
-        auto_error: bool = True,
+            self,
+            tokenUrl: str,
+            scheme_name: str = None,
+            scopes: dict = None,
+            auto_error: bool = True,
     ):
         if not scopes:
             scopes = {}
-        flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": scopes})
+        flows = OAuthFlowsModel(password={'tokenUrl': tokenUrl, 'scopes': scopes})
         super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
 
     async def __call__(self, request: Request) -> Optional[str]:
-        header_authorization: str = request.headers.get("Authorization")
-        cookie_authorization: str = request.cookies.get("Authorization")
+        header_scheme, header_param = get_authorization_scheme_param(request.headers.get('Authorization'))
+        cookie_scheme, cookie_param = get_authorization_scheme_param(request.cookies.get('Authorization'))
 
-        header_scheme, header_param = get_authorization_scheme_param(
-            header_authorization
-        )
-        cookie_scheme, cookie_param = get_authorization_scheme_param(
-            cookie_authorization
-        )
-
-        if header_scheme.lower() == "bearer":
+        if header_scheme.lower() == 'bearer':
             authorization = True
             scheme = header_scheme
             param = header_param
 
-        elif cookie_scheme.lower() == "bearer":
+        elif cookie_scheme.lower() == 'bearer':
             authorization = True
             scheme = cookie_scheme
             param = cookie_param
@@ -97,10 +86,10 @@ class OAuth2PasswordBearerCookie(OAuth2):
         else:
             authorization = False
 
-        if not authorization or scheme.lower() != "bearer":
+        if not authorization or scheme.lower() != 'bearer':
             if self.auto_error:
                 raise HTTPException(
-                    status_code=HTTP_403_FORBIDDEN, detail="Not authenticated"
+                    status_code=HTTP_403_FORBIDDEN, detail='Not authenticated'
                 )
             else:
                 return None
@@ -110,15 +99,16 @@ class OAuth2PasswordBearerCookie(OAuth2):
 class BasicAuth(SecurityBase):
     def __init__(self, scheme_name: str = None, auto_error: bool = True):
         self.scheme_name = scheme_name or self.__class__.__name__
+        self.model = SecurityBaseModel(type='http')
         self.auto_error = auto_error
 
     async def __call__(self, request: Request) -> Optional[str]:
-        authorization: str = request.headers.get("Authorization")
+        authorization: str = request.headers.get('Authorization')
         scheme, param = get_authorization_scheme_param(authorization)
-        if not authorization or scheme.lower() != "basic":
+        if not authorization or scheme.lower() != 'basic':
             if self.auto_error:
                 raise HTTPException(
-                    status_code=HTTP_403_FORBIDDEN, detail="Not authenticated"
+                    status_code=HTTP_403_FORBIDDEN, detail='Not authenticated'
                 )
             else:
                 return None
@@ -127,9 +117,9 @@ class BasicAuth(SecurityBase):
 
 basic_auth = BasicAuth(auto_error=False)
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-oauth2_scheme = OAuth2PasswordBearerCookie(tokenUrl="/token")
+oauth2_scheme = OAuth2PasswordBearerCookie(tokenUrl='/token')
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
@@ -142,45 +132,46 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
+def get_user(username: str) -> Optional[UserInDB]:
+    for row in db.query("SELECT * FROM UserTokens "
+                       f"WHERE UPPER([User]) LIKE '%{username.upper()}' "
+                        "ORDER BY Timestamp DESC"):
+        return UserInDB(**row)
+
+    logger.info(f'get_user failed: username: {username}')
 
 
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
+def authenticate_user(username: str, uid: str, netaddr: str) -> Optional[UserInDB]:
+    user = get_user(username)
     if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
+        return None
+    if not (user.UID == uid and user.NetAddress == netaddr and
+            user.Timestamp + timedelta(days=7) > datetime.utcnow()):
+        logger.info(f'authenticate_user failed: username: {username}, UID: {uid}, NetAddress: {netaddr}'
+                    f', DB UID: {user.UID}, DB NetAddress: {user.NetAddress}, DB UID Timestamp: {user.Timestamp}')
+        return None
     return user
 
 
 def create_access_token(*, data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    return jwt.encode(
+        {**data, 'exp': datetime.utcnow() + (expires_delta if expires_delta is not None
+                                             else timedelta(minutes=JWT_ACCESS_TOKEN_EXPIRE_MINUTES))},
+        JWT_SECRET_KEY, algorithm=JWT_ALGORITHM)
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
-        status_code=HTTP_403_FORBIDDEN, detail="Could not validate credentials"
-    )
+        status_code=HTTP_403_FORBIDDEN, detail='Could not validate credentials')
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[JWT_ALGORITHM])  # will raise error if token expired
+        username: str = payload.get('sub')
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
     except PyJWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = get_user(username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
@@ -188,85 +179,88 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
 async def get_current_active_user(current_user: User = Depends(get_current_user)):
     if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(status_code=400, detail='Inactive user')
     return current_user
 
 
-@app.get("/")
+@app.get('/')
 async def homepage():
-    return "Welcome to the security test!"
+    return 'Welcome to the API!'
 
 
-@app.post("/token", response_model=Token)
-async def route_login_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
+@app.post('/token', response_model=Token)
+async def route_login_access_token(request: Request, form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(username=form_data.username, uid=form_data.password, netaddr=request.client.host)
     if not user:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
+        raise HTTPException(status_code=400, detail='Incorrect username or password')
+    access_token = create_access_token(data=dict(sub=form_data.username))
+    return {'access_token': access_token, 'token_type': 'bearer'}
 
 
-@app.get("/logout")
+@app.get('/logout')
 async def route_logout_and_remove_cookie():
-    response = RedirectResponse(url="/")
-    response.delete_cookie("Authorization", domain="localtest.me")
+    response = RedirectResponse(url='/')
+    response.delete_cookie('Authorization', domain=COOKIE_DOMAIN_NAME)
     return response
 
 
-@app.get("/login_basic")
-async def login_basic(auth: BasicAuth = Depends(basic_auth)):
+@app.get('/login_basic')
+async def login_basic(request: Request, auth: BasicAuth = Depends(basic_auth)):
     if not auth:
-        response = Response(headers={"WWW-Authenticate": "Basic"}, status_code=401)
+        response = Response(headers={'WWW-Authenticate': 'Basic'}, status_code=401)
         return response
 
     try:
-        decoded = base64.b64decode(auth).decode("ascii")
-        username, _, password = decoded.partition(":")
-        user = authenticate_user(fake_users_db, username, password)
+        decoded = base64.b64decode(auth).decode('ascii')
+        username, _, password = decoded.partition(':')
+        user = authenticate_user(username=username, uid=password, netaddr=request.client.host)
         if not user:
-            raise HTTPException(status_code=400, detail="Incorrect email or password")
+            raise HTTPException(status_code=400, detail='Incorrect email or password')
 
-        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-        access_token = create_access_token(
-            data={"sub": username}, expires_delta=access_token_expires
-        )
-
+        access_token = create_access_token(data=dict(sub=username))
         token = jsonable_encoder(access_token)
 
-        response = RedirectResponse(url="/docs")
+        response = RedirectResponse(url='/docs')
         response.set_cookie(
-            "Authorization",
-            value=f"Bearer {token}",
-            domain="localtest.me",
+            'Authorization',
+            value=f'Bearer {token}',
+            domain=COOKIE_DOMAIN_NAME,
             httponly=True,
-            max_age=1800,
-            expires=1800,
+            max_age=60 * JWT_ACCESS_TOKEN_EXPIRE_MINUTES,  # lifetime of cookie in seconds
+            expires=60 * JWT_ACCESS_TOKEN_EXPIRE_MINUTES,  # number of seconds until cookie expires
         )
         return response
 
     except:
-        response = Response(headers={"WWW-Authenticate": "Basic"}, status_code=401)
+        response = Response(headers={'WWW-Authenticate': 'Basic'}, status_code=401)
         return response
 
 
-@app.get("/openapi.json")
+@app.get('/openapi.json', include_in_schema=False)
 async def get_open_api_endpoint(current_user: User = Depends(get_current_active_user)):
-    return JSONResponse(get_openapi(title="FastAPI", version=1, routes=app.routes))
+    return JSONResponse(get_openapi(title='JK API', version='0.1', routes=app.routes))
 
 
-@app.get("/docs")
+@app.get('/docs', include_in_schema=False)
 async def get_documentation(current_user: User = Depends(get_current_active_user)):
-    return get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
+    return get_swagger_ui_html(openapi_url='/openapi.json', title='docs')
 
 
-@app.get("/users/me/", response_model=User)
+@app.get('/me', response_model=User)
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
 
 
-@app.get("/users/me/items/")
-async def read_own_items(current_user: User = Depends(get_current_active_user)):
-    return [{"item_id": "Foo", "owner": current_user.username}]
+# @app.get('/users/me/items/')
+# async def read_own_items(current_user: User = Depends(get_current_active_user)):
+#     return [{'item_id': 'Foo', 'owner': current_user.username}]
+
+
+@app.get('/test')
+async def testme(current_user: User = Depends(get_current_active_user)):
+    return {'this': 100}
+
+
+import uvicorn
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=18078)
